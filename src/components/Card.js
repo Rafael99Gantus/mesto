594b48b63eddff22740ff6e1724bd7b5d1221976
Рафаэль Гантус <rafael.gantus@bk.ru>
2023-10-07 21:56:05 +0300

    fix

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,12 @@
 export class Card{
-  constructor(data, templateSelector, handleOpenPopup) {
+  constructor(data, templateSelector, handleOpenPopup, handleDeleteCard) {
     this._name = data.name;
     this._link = data.link;
     this._likes = data.likes.length;
     this._handleOpenPopup = handleOpenPopup;
     // this._numberlike = numberlike;
-    // this._id = data._id;
-    // this._handleDeleteCard = handleDeleteCard;
+    this._id = data._id;
+    this._handleDeleteCard = handleDeleteCard;
     this._templateSelector = templateSelector;
   }
 
@@ -15,11 +15,15 @@ export class Card{
     return card;
   }
 
+  _handleDelete(){
+    this._handleDeleteCard(this._id)
+    }
+
   generateCard() {
     this._element = this._getTemplate();
     this._likeIcon = this._element.querySelector('#first-heart');
     this._trashIcon = this._element.querySelector('.elements__trash');
-    this._trashButton = document.querySelector('#buttonAnswer');
+    this._trashButton = this._element.querySelector('#buttonAnswer');
     this._trashPopup = document.querySelector('#answerPopup');
     this._imageCard = this._element.querySelector('.elements__image');
     this._nameCard = this._element.querySelector('.elements__name');
@@ -40,8 +44,6 @@ export class Card{
   }
 
   _setEventListeners() {
-    // const deleteCard = this._newCard.querySelector('#buttonAnswer')
-    // deleteCard.addEventListener('click', () => {this._handleDelete();});
 
     this._likeIcon.addEventListener('click', () => {
       this._handleLike();
@@ -49,6 +51,8 @@ export class Card{
 
     this._trashIcon.addEventListener('click', () => {
       this._handleTrashButton();
+      const deleteCard = this._element.querySelector('#buttonAnswer')
+      deleteCard.addEventListener('click', () => {this._handleDelete();});
     });
 
     this._imageCard.addEventListener('click', () => {
@@ -60,17 +64,12 @@ export class Card{
     this._likeIcon.classList.toggle('elements__heart_active');
   }
 
-//Добавление +1 к количеству лайков
-  _counterLike(){
-    
-  }
 //Открытие попапа ANSWER
   _handleTrashButton() {
     this._trashPopup.classList.add('popup_opened');
   }
 
-  //Удаление карточки
-  _handleDelete(){
-    this._handleDeleteCard(this._id)
+  delete () {
+    this._element.remove();
   }
 }
